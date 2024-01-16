4fb573d45f50be7848e31d80e36cd41a8114258e
di3g0r <a01741413@tec.mx>
2024-01-15 17:57:01 -0700

    gui settings

---
+++ rgbtohex.py
@@ -0,0 +1,2 @@
+def _from_rgb(rgb):
+    return "#%02x%02x%02x" % rgb   
--- ventana.py
+++ ventana.py
@@ -1,4 +1,18 @@
 import tkinter as tk
+from rgbtohex import _from_rgb
 
+#Initial configuration
 window = tk.Tk()
+window.title('Swap')
+window.geometry('650x400')
+bgcolor = _from_rgb((15,114,219))
+window.configure(bg = bgcolor)
+
+welcome = tk.Label(window, text= 'SWAP', bg = bgcolor,  justify = 'center',fg = "white", font = ('Arial', 24))
+welcome.place(x = 230, y = 40)  
+welcome.pack()
+
+window.resizable(True, True) 
+
+window.mainloop()
 
